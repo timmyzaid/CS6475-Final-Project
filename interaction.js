01fb72a15ad8b0136f182eb488eb5b87b75da96c
timmyzaid <tzm293@msn.com>
2015-04-18 15:00:47 -0400

    Image Upload
    Added functionality for uploading a series of images and then placing
    them in a /temp/ directory.  Uses async transfer via jQuery and ajax
    along with node library formidable to parse the resulting data.

--- interaction.js
+++ interaction.js
@@ -1,4 +1,36 @@
-function handleFileSelect(evt){
-	var files = evt.target.files;
-	//var k = 12;
+function handleFileSelect(event){
+	//var form = document.getElementById('file-form');
+	//var fileSelect = document.getElementById('file-select');
+	//var uploadButton = document.getElementById('upload-button');
+
+	//form.onsubmit = function(event) {
+		event.preventDefault();
+		var files = event.target.files;
+		if(!files.length)
+			return;
+
+		var formData = new FormData();
+		//uploadButton.innerHTML = 'Uploading...';
+
+		for(var i = 0; i < files.length; i++){
+			var file = files[i];
+
+			if(!file.type.match('image.*'))
+				continue;
+
+			formData.append('tilePhotos', file, file.name);
+		}
+
+		var xhr = new XMLHttpRequest();
+		xhr.open('post', 'files', true);
+
+		xhr.onload = function(){
+			if(xhr.status === 200)
+				console.log("Success!");
+			else
+				alert('Upload failed');
+		};
+
+		xhr.send(formData);
+	//}
 }
--- server.js
+++ server.js
@@ -1,35 +1,57 @@
 var http = require('http'),
-	fs = require('fs');
+	fs = require('fs'),
+	formidable = require('formidable');
 
 
-http.createServer(function(request, response) {
-	//console.log(request.url);
-	if(request.url === '/'){
-		fs.readFile("./index.html", function(err, html){
-			if(err)
-				throw err;
-			response.writeHeader(200, {"Content-Type": "text/html"});
-			response.write(html);
-			response.end();
-		});
+var server = http.createServer(function(request, response) {
+	console.log(request.url);
+	if(request.method === "GET"){
+		if(request.url === '/'){
+			fs.readFile("./index.html", function(err, html){
+				if(err)
+					throw err;
+				response.writeHeader(200, {"Content-Type": "text/html"});
+				response.write(html);
+				response.end();
+			});
+		}
+		else if(request.url === "/layout.css"){
+			fs.readFile("./layout.css", function(err, layout){
+				if(err)
+					throw err;
+				response.writeHeader(200, {"Content-Type": "text/css"});
+				response.write(layout);
+				response.end();
+			});
+		}
+		else if(request.url === "/interaction.js"){
+			fs.readFile("./interaction.js", function(err, js){
+				if(err)
+					throw err;
+				response.writeHeader(200, {"Content-Type": "text/javascript"});
+				response.write(js);
+				response.end();
+			});
+		}
 	}
-	else if(request.url === "/layout.css"){
-		fs.readFile("./layout.css", function(err, layout){
-			if(err)
-				throw err;
-			response.writeHeader(200, {"Content-Type": "text/css"});
-			response.write(layout);
-			response.end();
-		});
-	}
-	else if(request.url === "/interaction.js"){
-		fs.readFile("./interaction.js", function(err, js){
-			if(err)
-				throw err;
-			response.writeHeader(200, {"Content-Type": "text/javascript"});
-			response.write(js);
-			response.end();
-		});
+	else if(request.method === "POST"){
+		console.log("received post");
+		if(request.url === "/files"){
+			var form = new formidable.IncomingForm();
+			form.uploadDir = './temp/';
+			form.keepExtensions = true;
+			form.parse(request);
+
+			form.on('fileBegin', function(name, file) {
+				file.path = './temp/' + file.name;
+			});
+			
+			form.on('end', function(fields, files) {
+				console.log("Upload comleted!");
+				response.writeHead(200);
+				response.end();
+			});
+		}
 	}
 }).listen(8000);
 console.log('Server running at http://127.0.0.1:8000/');
