e99248dcfadf0edb732883454f07cef942693791
timmyzaid <tzm293@msn.com>
2015-04-19 15:54:56 -0400

    Mosaic Generation
    Generate the mosaic by finding the image with the closest average to the
    original tile.  Tint the result  to better match the color value of the
    base image.

--- Mosaic/generateMosaic.py
+++ Mosaic/generateMosaic.py
@@ -49,7 +49,7 @@ def calculateAverageRGB(image):
 class ImageInfo:
     def __init__(self, avg):
         self.rgbAvg = avg
-        self.count = 0
+        self.count = 1
     
 def createAverageRGBList(images):
     print "Creating average RGB list"
@@ -59,10 +59,38 @@ def createAverageRGBList(images):
 
     return averages
 
-def findClosestMatch(tileAvg, tileImages):
-    return tileImages[0]
+def findClosestMatch(baseTileAvg, tileAvgs):
+    difference = 0
+    index = 0
+    returnIndex = 0
+    for tile in tileAvgs:
+        testDifference = tile.count * (abs(baseTileAvg[0] - tile.rgbAvg[0]) + abs(baseTileAvg[1] - tile.rgbAvg[1]) + abs(baseTileAvg[2] - tile.rgbAvg[2]))
+        if difference == 0 or testDifference < difference:
+            difference = testDifference
+            returnIndex = index
+        index += 1
+
+    tileAvgs[returnIndex].count += 1
+    return returnIndex
     
-def generateMosaic(baseImage, tileImages, mosaicSize):
+def tintImageChannel(image, diff):
+    image = image + diff
+    np.place(image, image > 255, 255)
+    np.place(image, image < 0, 0)
+    return image
+
+def tintImage(baseTileAvg, tileAvg, tileImage):
+    redDiff = baseTileAvg[0] - tileAvg[0]
+    greenDiff = baseTileAvg[1] - tileAvg[1]
+    blueDiff = baseTileAvg[2] - tileAvg[2]
+    
+    returnImage = np.copy(tileImage)
+    returnImage[:, :, 0] = tintImageChannel(np.int16(tileImage[:, :, 0]), blueDiff)
+    returnImage[:, :, 1] = tintImageChannel(np.int16(tileImage[:, :, 1]), greenDiff)
+    returnImage[:, :, 2] = tintImageChannel(np.int16(tileImage[:, :, 2]), redDiff)
+    return returnImage
+
+def generateMosaic(baseImage, tileImages, tileAvgs, mosaicSize):
     print "Creating mosaic"
     
     tileSize = tileImages[0].shape[0]
@@ -76,11 +104,9 @@ def generateMosaic(baseImage, tileImages, mosaicSize):
         iCol = 0
         iResultCol = 0
         while iCol < iCols:
-            if iResultCol == 1000:
-                k = 12
-            tileAvg = calculateAverageRGB(baseImage[iRow : iRow + mosaicSize, iCol : iCol + mosaicSize, :])
-            #print "iResultRow: " + str(iResultRow) + " - iResultCol: " + str(iResultCol) + " - iRow: " + str(iRow) + " - iCol: " + str(iCol)
-            resultImage[iResultRow : iResultRow + tileSize, iResultCol : iResultCol + tileSize, :] = findClosestMatch(tileAvg, tileImages)
+            baseTileAvg = calculateAverageRGB(baseImage[iRow : iRow + mosaicSize, iCol : iCol + mosaicSize, :])
+            matchIndex = findClosestMatch(baseTileAvg, tileAvgs)
+            resultImage[iResultRow : iResultRow + tileSize, iResultCol : iResultCol + tileSize, :] = tintImage(baseTileAvg, tileAvgs[matchIndex].rgbAvg, tileImages[matchIndex])
             iCol += mosaicSize
             iResultCol += tileSize
         
@@ -102,5 +128,7 @@ if __name__ == "__main__":
     print str(len(images))
     baseImage = readBaseImage()
     
-    mosaic = generateMosaic(baseImage, images, 50)
+    mosaic = generateMosaic(baseImage, images, averages, 50)
+    
+    print "Writing mosaic"
     cv2.imwrite("test.jpg", mosaic)
